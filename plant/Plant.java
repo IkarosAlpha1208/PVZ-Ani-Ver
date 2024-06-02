6fa5febaaf7b8ef42eddec6a08f2fec748fb6708
IkarosAlpha1208 <davidxue1978@gmail.com>
2024-06-02 16:30:40 -0400

    plant

--- plant/Plant.java
+++ plant/Plant.java
@@ -1,5 +1,23 @@
 package plant;
 
-public class Plant {
+abstract class Plant {
     private int hp;
+    private int atk;
+    private int atkSpd;
+    private int cooldown;
+    private int cost;
+
+    public Plant(int hp, int atk, int atkSpd, int cooldown, int cost){
+        this.hp = hp;
+        this.atk = atk;
+        this.atkSpd = atkSpd;
+        this.cooldown = cooldown;
+        this.cost = cost;
+    }
+
+    abstract void attack();
+
+    public void takeDmg(int damage){
+        this.hp -= damage;
+    }
 }
