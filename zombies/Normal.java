94bf22dae31531307d719d702b2ecfd70ceac36f
IkarosAlpha1208 <davidxue1978@gmail.com>
2024-06-05 13:59:59 -0400

    Revert "Merge branch 'main' of https://github.com/IkarosAlpha1208/PVZ-Ani-Ver"
    This reverts commit 18daaf66137595e0bfafb45f6f065dd68f06f1e6.

--- zombies/Normal.java
+++ zombies/Normal.java
@@ -1,9 +1,7 @@
 package zombies;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
 public class Normal extends Zombie {
+    // BufferedImage walking = new BufferedImage
 
     public Normal(int hp, int atk, int atkspd, int speed, int x, int y) {
         super(hp, atk, atkspd, speed, x, y);
@@ -18,23 +16,17 @@ public class Normal extends Zombie {
 
     }
 
-    public void setX(int x) {
-        this.x = x;
-
-    }
-
     @Override
     public void attack() {
         // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'attack'");
+        throw new UnsupportedOperationException("Unimplemented method 'attack'");
     }
 
     @Override
     public void move() {
-        this.x = this.x - 1;
-        this.hitX = this.hitX - 1;
+        // TODO Auto-generated method stub
 
-        // throw new UnsupportedOperationException("Unimplemented method 'move'");
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
     }
 
 }
